3a4b7644725bf2b9804b99b14e885d9f0e2de98e
zhuhuiting <zhuhuiting@yy.com>
2023-08-11 07:54:55 +0800

    feat: challenge2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -46,3 +46,109 @@ checkWinner(avgDolphins, avgKoalas)
 const avgDolphins1 = calcAverage(85, 54, 41)
 const avgKoalas1 = calcAverage(23, 34, 27)
 checkWinner(avgDolphins1, avgKoalas1)
+
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+GOOD LUCK 😀
+*/
+
+const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2
+console.log(calcTip(100));
+
+const bills = [125, 555, 44]
+const tips = []
+tips.push(calcTip(bills[0]))
+tips.push(calcTip(bills[1]))
+tips.push(calcTip(bills[2]))
+console.log(tips);
+const total = bills.map(bill => bill + calcTip(bill))
+console.log(total)
+
+
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven'],
+  hasDriversLicense: true,
+
+  getSummary: function () {
+    return `Jonas is a ${this.age} - year old teacher, and he has ${this.hasDriversLicense ? "a" : 'no'} driver's license`
+  }
+};
+
+// "Jonas has 3 friends, and his best friend is called Michael"
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
+
+// Jonas is a 46 - year old teacher, and he has a driver's license
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/*
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+
+GOOD LUCK 😀
+*/
+
+const mark = {
+  firstName: 'Mark',
+  lastName: 'Miller',
+  mass: 78,
+  heigh: 1.69,
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`
+  },
+  calcBMI: function () {
+    return this.mass / (this.heigh * this.heigh)
+  }
+}
+
+const john = {
+  firstName: 'John',
+  lastName: 'Smith',
+  mass: 92,
+  heigh: 1.95,
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`
+  },
+  calcBMI: function () {
+    return this.mass / (this.heigh * this.heigh)
+  }
+}
+let higherBMI
+let higherFullName
+let lowerBMI
+let lowerFullName
+if (mark.calcBMI() > john.calcBMI()) {
+  higherBMI = mark.calcBMI()
+  higherFullName = mark.getFullName()
+  lowerBMI = john.calcBMI()
+  lowerFullName = john.getFullName()
+} else {
+  higherBMI = john.calcBMI()
+  higherFullName = john.getFullName()
+  lowerBMI = mark.calcBMI()
+  lowerFullName = mark.getFullName()
+}
+const result = `${higherFullName}'s BMI (${higherBMI}) is higher than ${lowerFullName}'s (${lowerBMI})!`
+console.log('result', result);
