cf037e53e64f39097b0c1e9d9cd368fb6d6126f7
zhuhuiting <zhuhuiting@yy.com>
2023-08-13 17:47:00 +0800

    feat: challenge1

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -19,9 +19,13 @@ TEST DATA 2: [12, 5, -5, 0, 4]
 
 
 function printForecast(arr) {
+  const temps = []
   for (let i = 0; i < arr.length; i++) {
-    console.log(`${arr[i]}ºC in ${i + 1} days`);
+    // console.log(`${arr[i]}ºC in ${i + 1} days`);
+    temps.push(`...${arr[i]}ºC in ${i + 1} days`)
   }
+
+  return temps.join(' ')
 }
 
 printForecast([17, 21, 23])
