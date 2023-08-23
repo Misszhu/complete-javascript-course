83a72c06c3ef33f99a98705f672a3c23b9052046
zhuhuiting <zhuhuiting@shoplineapp.com>
2023-08-23 20:34:51 +0800

    feat: modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const modalEl = document.querySelector('.modal')
+const overlayEl = document.querySelector('.overlay')
+const closeModalBtn = document.querySelector('.close-modal')
+
+const showModalBtnList = document.querySelectorAll('.show-modal')
+
+function showModal() {
+  modalEl.classList.remove('hidden')
+  overlayEl.classList.remove('hidden')
+}
+
+function hideModal() {
+  modalEl.classList.add('hidden')
+  overlayEl.classList.add('hidden')
+}
+
+for(let i = 0; i < showModalBtnList.length; i++) {
+  showModalBtnList[i].addEventListener('click', () => {
+    showModal()
+  })
+}
+
+closeModalBtn.addEventListener('click', () => {
+  hideModal()
+})
+
+document.addEventListener('keydown', (event) => {
+  console.log(event);
+  if (event.key === 'Escape') {
+    hideModal()
+  }
+})
