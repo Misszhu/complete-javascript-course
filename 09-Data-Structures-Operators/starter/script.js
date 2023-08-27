b768b5ae737d224c8b10752732140fc7646d0341
zhuhuiting <zhuhuiting@yy.com>
2023-08-27 18:50:11 +0800

    feat: challenge1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -27,3 +27,64 @@ const restaurant = {
     },
   },
 };
+
+
+
+// code challenge 1
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const [players1, players2] = game.players
+const [gk1, ...fieldPlayers1] = players1
+const [gk2, ...fieldPlayers2] = players2
+const allPlayers = [...players1, ...players2]
+const players1Final = [...players2, 'Thiago', 'Coutinho', 'Perisic']
+const { odds: { team1, x: draw, team2 } } = game
+
+function printGoals(...players) {
+  console.log(`${players.length} goals were scored`);
+
+}
+
+printGoals(...game.scored)
+
+team1 > team2 && console.log('Team 1 is more likely to win');
+team1 < team2 && console.log('Team 2 is more likely to win');
